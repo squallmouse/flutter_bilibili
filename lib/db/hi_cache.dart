103d83bbf35ff0a650829a8464eb66dc2c931a14
squallmouse <squallmouse@gmail.com>
2022-06-11 08:48:35 +0800

    2.13--本地存储 hi_cache

---
+++ lib/db/hi_cache.dart
@@ -0,0 +1,64 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// 缓存管理类
+class HiCache {
+  SharedPreferences? prefs;
+  // 私有化的构造方法
+  HiCache._() {
+    init();
+  }
+  void init() async {
+    if (prefs == null) {
+      prefs = await SharedPreferences.getInstance();
+    }
+  }
+
+  // 单例
+  static HiCache? _instance;
+  static HiCache getInstance() {
+    if (_instance == null) {
+      _instance = HiCache._();
+    }
+    return _instance!;
+  }
+
+  // 预初始化
+  static Future<HiCache> preInit() async {
+    if (_instance == null) {
+      var prefs = await SharedPreferences.getInstance();
+      _instance = HiCache._pre(prefs);
+    }
+    return _instance!;
+  }
+
+  HiCache._pre(SharedPreferences prefs) {
+    this.prefs = prefs;
+  }
+
+  //*  ------------------------------ */
+  //*   方法
+
+  setString(String key, String value) {
+    prefs?.setString(key, value);
+  }
+
+  setDouble(String key, double value) {
+    prefs?.setDouble(key, value);
+  }
+
+  setInt(String key, int value) {
+    prefs?.setInt(key, value);
+  }
+
+  setBool(String key, bool value) {
+    prefs?.setBool(key, value);
+  }
+
+  setStringList(String key, List<String> value) {
+    prefs?.setStringList(key, value);
+  }
+
+  Object? get(String key) {
+    return prefs?.get(key);
+  }
+}
